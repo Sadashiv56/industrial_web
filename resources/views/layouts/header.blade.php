0d046be6549ef72e2c1d53242c1b780756d166e9
Atul Chourasiya <atulchourasiya73@gmail.com>
2024-04-24 20:47:43 +0530

    Created Front Page

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,34 +1,143 @@
-<header class="bg-white">
-    <nav class="flex justify-between items-center w-[92%]  mx-auto">
-        <div>
-            <img class="w-16 cursor-pointer" src="https://seeklogo.com/images/C/corporate-company-logo-749CEE6ADC-seeklogo.com.png" alt="...">
+<header id="header" id="home">
+    <div class="header-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-sm-6 col-4 header-top-left no-padding">
+                    <ul>
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                        <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-6 col-sm-6 col-8 header-top-right no-padding">
+                    <a href="tel:+880 012 3654 896">+880 012 3654 896</a>
+                    <a
+                        href="https://preview.colorlib.com/cdn-cgi/l/email-protection#45363035352a373105262a292a37292c276b262a28"><span
+                            class="__cf_email__"
+                            data-cfemail="e89b9d9898879a9ca88b8784879a84818ac68b8785">[email&#160;protected]</span></a>
+                </div>
+            </div>
         </div>
-        <div>
-          <div
-              class="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5">
-              <ul class="flex md:flex-row flex-col md:items-center">
-                  <li class="p-5">
-                      <a class="hover:text-gray-500 " href="#">About Us</a>
-                  </li>
-                  <li class="p-5">
-                      <a class="hover:text-gray-500 " href="#">Products</a>
-                  </li>
-                  <li class="p-5">
-                      <a class="hover:text-gray-500 " href="#">Applications</a>
-                  </li>
-                  <li class="p-5">
-                      <a class="hover:text-gray-500 " href="#">Our Associates</a>
-                  </li>
-                  <li class="p-5">
-                      <a class="hover:text-gray-500 " href="#">Request For Quotation</a>
-                  </li>
-                  <li class="p-5">
-                      <a class="hover:text-gray-500 " href="#">Contact Us</a>
-                  </li>
-              </ul>
-          </div>
-          <div class="flex items-center gap-6">
-              <ion-icon onclick="onToggleMenu(this)" name="menu" class="text-3xl cursor-pointer md:hidden"></ion-icon>
-          </div>
+    </div>
+    <div class="container main-menu">
+        <div class="row align-items-center justify-content-between d-flex">
+            <div id="logo">
+                <a href="index-2.html"><img src="img/logo.png" style="width: 4.5rem; height: 2.5rem" alt title /></a>
+            </div>
+            <nav id="nav-menu-container">
+                <ul class="nav-menu">
+                    <li class="nav-menu-item" ><a href="about.html">About us</a></li>
+                    <li class="menu-has-children nav-menu-item "><a class="sf-arrows" href>Products</a>
+                        <ul class="nav-menu-dropdown">
+                            <li>
+                                <div class="d-flex flex-column p-4">
+                                    <div class="p-2 bg-muted">Heading</div>
+                                    <div class="p-1 nav-menu-dropdown-item">Home</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">About</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="d-flex flex-column p-4">
+                                    <div class="p-2 bg-muted">Heading</div>
+                                    <div class="p-1 nav-menu-dropdown-item">Home</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">About</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="d-flex flex-column p-4">
+                                    <div class="p-2 bg-muted">Heading</div>
+                                    <div class="p-1 nav-menu-dropdown-item">Home</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">About</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="d-flex flex-column p-4">
+                                    <div class="p-2 bg-muted">Heading</div>
+                                    <div class="p-1 nav-menu-dropdown-item">Home</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">About</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-menu-item" ><a href="projects.html">Applications</a>
+                        <ul class="nav-menu-dropdown">
+                            <li>
+                                <div class="d-flex flex-column p-4">
+                                    <div class="p-2 bg-muted">Heading</div>
+                                    <div class="p-1 nav-menu-dropdown-item">Home</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">About</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="d-flex flex-column p-4">
+                                    <div class="p-2 bg-muted">Heading</div>
+                                    <div class="p-1 nav-menu-dropdown-item">Home</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">About</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="d-flex flex-column p-4">
+                                    <div class="p-2 bg-muted">Heading</div>
+                                    <div class="p-1 nav-menu-dropdown-item">Home</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">About</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="d-flex flex-column p-4">
+                                    <div class="p-2 bg-muted">Heading</div>
+                                    <div class="p-1 nav-menu-dropdown-item">Home</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">About</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                    <hr>
+                                    <div class="p-1 nav-menu-dropdown-item">Content</div>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu-has-children nav-menu-item "><a href>Our assiociate</a>
+                    </li>
+                    <li class="nav-menu-item"><a href="contact.html">Request for quotation</a></li>
+                    <li class="nav-menu-item"><a href="elements.html">Contact us</a></li>
+                </ul>
+            </nav>
         </div>
+    </div>
 </header>
